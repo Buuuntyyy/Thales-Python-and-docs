cbd22deee71f2e56cc113cfc4d1583547228d6a1
Buuuntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2023-03-06 13:24:09 +0100

    Update thalès.py
    loop until the ethernet_resultdata is entirely read
    Works, but need to be optimized

--- thalès.py
+++ thalès.py
@@ -50,13 +50,13 @@ def frame_date(liste) -> float:
     date_time = datetime.fromtimestamp(time)
 
 #permet de lire la taille du paquet en octet
-def taille_paquet(liste) -> int:
-    array = readBitsASoctet(liste, 24, 28)
+def taille_paquet(liste, decalage) -> int:
+    array = readBitsASoctet(liste, decalage + 24, decalage + 28)  
     array = array[::-1]
     return conv2dec(array)
 
-def lire_addr_mac(liste) -> int: #octet 28 à 40
-    bdata = readBitsASoctet(liste, 28, 40)
+def lire_addr_mac(liste, decalage) -> int: #octet 28 à 40
+    bdata = readBitsASoctet(liste, decalage + 28, decalage + 40)
     #print(f"bdata = {bdata}")
     hex = bin2hex(bdata)
     addr1 = hex[0:13]
@@ -69,14 +69,14 @@ def lire_addr_mac(liste) -> int: #octet 28 à 40
         d_addr += addr2[i:i+2] + ":"
     s_addr = s_addr[0:17]
     d_addr = d_addr[0:17]
-    print(f"adresses : addr1 = {s_addr}, addr2 = {d_addr}")
+    #print(f"adresses : addr1 = {s_addr}, addr2 = {d_addr}")
 
     return s_addr, d_addr
 
-def lire_addr_ip(liste) -> str: #octet 54 à 62
+def lire_addr_ip(liste, decalage) -> str: #octet 54 à 62
     addrIp1 = ""
     addrIp2 = ""
-    bdata = readBitsASoctet(liste, 54, 62)
+    bdata = readBitsASoctet(liste, decalage + 54, decalage + 62)
     addresses = conv_ip(bdata)
     s_addr = addresses[0]
     d_addr = addresses[1]
@@ -86,21 +86,21 @@ def lire_addr_ip(liste) -> str: #octet 54 à 62
         addrIp2 += str(element) + "."
     addrIp1 = addrIp1[0:13]
     addrIp2 = addrIp2[0:13]
-    print(addrIp1, addrIp2)
+    #print(addrIp1, addrIp2)
 
     return addrIp1, addrIp2
 
 #lire packet date
-def packet_date(fields_liste):
+def packet_date(fields_liste, decalage):
     packet_dateListe = []
-    for i in range(19, 22):
+    for i in range(decalage + 19,decalage + 22):
         packet_dateListe.append(fields_liste[i])
 
 
 def conv_ip(liste) -> tuple:
     octet_val_ip1 = []
     ipList = order_octet(liste)
-    print(ipList)
+    #print(ipList)
     val = 0
     octet_val_ip2 = []
 
@@ -121,72 +121,72 @@ def conv_ip(liste) -> tuple:
         octet_val_ip2.append(val)    
     return octet_val_ip1, octet_val_ip2    
 
-def lire_fields(liste) -> list:
+def lire_fields(liste, decalage) -> list:
     fields = []
-    fields.append(readBitsASoctet(liste, 40, 42)) #field 1
-    fields.append(readBitsASoctet(liste, 42, 44)) #field 2
-    fields.append(readBitsASoctet(liste, 44, 46)) #field 3
-    fields.append(readBitsASoctet(liste, 46, 48)) #field 4
-    fields.append(readBitsASoctet(liste, 48, 50)) #field 5
-    fields.append(readBitsASoctet(liste, 50, 51)) #field 6
+    fields.append(readBitsASoctet(liste, decalage + 40, decalage +42)) #field 1
+    fields.append(readBitsASoctet(liste, decalage +42, decalage +44)) #field 2
+    fields.append(readBitsASoctet(liste, decalage +44, decalage +46)) #field 3
+    fields.append(readBitsASoctet(liste, decalage +46, decalage +48)) #field 4
+    fields.append(readBitsASoctet(liste, decalage +48, decalage +50)) #field 5
+    fields.append(readBitsASoctet(liste, decalage +50, decalage +51)) #field 6
     #octets IP et à ignorer
-    fields.append(readBitsASoctet(liste, 62, 64)) #field 9
-    fields.append(readBitsASoctet(liste, 64, 66)) #field 10
-    fields.append(readBitsASoctet(liste, 66, 68)) #field 11
+    fields.append(readBitsASoctet(liste, decalage +62, decalage +64)) #field 9
+    fields.append(readBitsASoctet(liste, decalage +64, decalage +66)) #field 10
+    fields.append(readBitsASoctet(liste, decalage +66, decalage +68)) #field 11
 
     #octets 68 à 70 => ignorer, 70 à 74 séparés en plusieurs fieds/FT
     #octets 70 à 72 (71 et 72 inclus) : fields 14, 16, 17, 18
         #field 14 : 1 bit (4ème bit de l'octet)
-    fields.append(readBitsASoctet(liste, 71, 72)) # => bit 564 #liste[563]
+    fields.append(readBitsASoctet(liste, decalage +71, decalage +72)) # => bit 564 #liste[563]
         #field 16 : 3 bits (Bits 6 à 8 inclus)
-    fields.append(readBitsASoctet(liste, 71, 722)) #liste[564:566]
+    fields.append(readBitsASoctet(liste, decalage +71, decalage +72)) #liste[564:566]
         #field 17 : 3 bits (Bits 9 à 11 inclus)
-    fields.append(readBitsASoctet(liste, 71, 72)) #liste[567:569]
+    fields.append(readBitsASoctet(liste, decalage +71, decalage +72)) #liste[567:569]
         #field 18 : 5 bits (Bits 12 à 16 inclus)
-    fields.append(readBitsASoctet(liste, 71, 72)) #liste[570:574]
+    fields.append(readBitsASoctet(liste, decalage +71, decalage +72)) #liste[570:574]
     #otets 72 à 74 (73 et 74 inclus) : field 20
         #field 20 : 14 bits (bits 3 à 16 inclus)
-    fields.append(readBitsASoctet(liste, 71, 72)) #liste[578:594]
+    fields.append(readBitsASoctet(liste, decalage +71, decalage +72)) #liste[578:594]
 
-    fields.append(readBitsASoctet(liste, 74, 76)) #field 21
+    fields.append(readBitsASoctet(liste, decalage +74, decalage +76)) #field 21
 
     #field 22 à ignorer
 
     #octets 76 à 77 (77 inclu seulement) : field 23, 25, 26
         #field 23 : 1 bit (5eme bit)
-    fields.append((liste[613]))
+    fields.append((liste[decalage +613]))
         #field 25 : 1 bit (7eme bit)
-    fields.append((liste[614]))
+    fields.append((liste[decalage +614]))
         #field 26 : 1 bit (8eme bit)
-    fields.append((liste[615]))
+    fields.append((liste[decalage +615]))
     #octets 77 à 78 (78 inclu seulement): field 27, 28
         #field 27 : 2 bits (2 premiers bits)
-    fields.append((liste[616:617]))
+    fields.append((liste[decalage +616:decalage +617]))
         #field 28 : 6 bits (6 derniers bits)
-    fields.append((liste[618:623]))
+    fields.append((liste[decalage +618:decalage +623]))
     #octet 78 à 80 (79 et 80 inclus) : fields 29, 30
         #field 29 : 6 bits (6 premiers bits)
-    fields.append((liste[624:629]))
+    fields.append((liste[decalage +624:decalage +629]))
         #field 30 : 10 bits (10 derniers bits)
-    fields.append((liste[630:639]))
+    fields.append((liste[decalage +630:decalage +639]))
     #octet 81 à ignorer
 
-    fields.append(readBitsASoctet(liste, 81, 82)) #octet 82 -> field 32 & FT_1
-    fields.append(readBitsASoctet(liste, 82, 84)) #octet 83 et 84 === field 33
-    fields.append(readBitsASoctet(liste, 84, 86)) #octets 85 et 86 === field 34 #83 à 88 = packet_date
-    fields.append(readBitsASoctet(liste, 86, 88)) #octets 87 et 88 === field 35
+    fields.append(readBitsASoctet(liste, decalage +81, decalage +82)) #octet 82 -> field 32 & FT_1
+    fields.append(readBitsASoctet(liste, decalage +82, decalage +84)) #octet 83 et 84 === field 33
+    fields.append(readBitsASoctet(liste, decalage +84, decalage +86)) #octets 85 et 86 === field 34 #83 à 88 = packet_date
+    fields.append(readBitsASoctet(liste, decalage +86, decalage +88)) #octets 87 et 88 === field 35
 
     return fields
 
-def lire_FT(liste) -> list:
+def lire_FT(liste, decalage) -> list:
     FT_val = []
-    FT_val.append(liste[172:176]) #FT_0
-    FT_val.append(liste[645:653]) #FT_1
-    FT_val.append(liste[570:575]) #FT_2
-    FT_val.append(liste[617:623]) #FT_3
-    FT_val.append(liste[623:629]) #FT_4
-    FT_val.append(liste[567:580]) #FT_5
-    FT_val.append(liste[563]) #FT_7
+    FT_val.append(liste[decalage +172:decalage +176]) #FT_0
+    FT_val.append(liste[decalage +645:decalage +653]) #FT_1
+    FT_val.append(liste[decalage +570:decalage +575]) #FT_2
+    FT_val.append(liste[decalage +617:decalage +623]) #FT_3
+    FT_val.append(liste[decalage +623:decalage +629]) #FT_4
+    FT_val.append(liste[decalage +567:decalage +580]) #FT_5
+    FT_val.append(liste[decalage +563]) #FT_7
 
     return FT_val
 
@@ -207,7 +207,7 @@ def bin2deci(liste) -> int:
         for i in range(0, len()):
             if inv[i] == 1:
                 val += 2**i
-    print(f"val = {val}")
+    #print(f"val = {val}")
     return val
 
 #permet de convertir 1 octet donné en hexadécimal, la liste en entrée doit être préalablement inversée
@@ -224,7 +224,7 @@ def bin2hex(byte) -> str:
             res1 += n * 2**j
             j += 1
         hexval = hex(res1)
-        print(f"demi octet : {temp}, val : {hexval[2::]}")
+        #print(f"demi octet : {temp}, val : {hexval[2::]}")
 
         chaine += hexval[2::]
     
@@ -233,16 +233,18 @@ def bin2hex(byte) -> str:
 def extracteur() -> tuple:
     path = "C:\\Users\\barfl\\Desktop\\saé_thalès\\ethernet.result_data"
     file_bin = read_binary_file_bits(path) #on garde le fichier binaire en mémoire pour rapidement y accéder et ne le lire qu'une seule fois
+    decalage = 0 #on initialise la variable de decalage à 0
     #secondes = frame_date(file_bin)
-    size = taille_paquet(file_bin)
-    macs = lire_addr_mac(file_bin)
-    ips = lire_addr_ip(file_bin)
-    date = packet_date(file_bin)
-    fields = lire_fields(file_bin)
-    FT = lire_FT(file_bin)
-    FT6 = lire_FT6(file_bin, fields)
-
-    
+    #il faut boucler sur toute la trame --> while True:
+    #il faut aussi appliquer un decalage à chaque fonction de lecture sauf FT6
+    while True:
+        size = taille_paquet(file_bin, decalage)
+        macs = lire_addr_mac(file_bin, decalage)
+        ips = lire_addr_ip(file_bin, decalage)
+        date = packet_date(file_bin, decalage)
+        fields = lire_fields(file_bin, decalage)
+        FT = lire_FT(file_bin, decalage)
+        FT6 = lire_FT6(file_bin, fields)
 
     return size, macs, ips, date, FT, FT6
 
